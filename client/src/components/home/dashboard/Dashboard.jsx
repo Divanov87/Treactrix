aea465f7994cdf89c1f157e9c4eb5c9f3fec8ec4
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-03 23:52:52 +0300

    Updated Dashboard with Top Rated Events section

--- client/src/components/home/dashboard/Dashboard.jsx
+++ client/src/components/home/dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import MainTop from "./main-top/MainTop.jsx";
 import MainService from "./main-service/MainService.jsx";
 import MainPinned from "./main-pinned/MainPinned.jsx";
+import MainRated from "./main-rated/MainRated.jsx";
+
 
 
 export default function Dashboard() {
@@ -9,6 +11,8 @@ export default function Dashboard() {
             <MainTop />
             <MainService />
             <MainPinned />
+            <MainRated />
+
         </>
     )
 }
