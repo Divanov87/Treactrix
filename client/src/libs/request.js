4e053e158539d10e699fb9a91cf2fa570bb9e984
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-03 19:16:29 +0300

    Added helper library for fetching data

---
+++ client/src/libs/request.js
@@ -0,0 +1,50 @@
+import Cookies from 'js-cookie';
+
+const options = (data) => {
+    const options = {
+        headers: {},
+        credentials: 'include',
+    };
+
+    if (data) {
+        options.body = JSON.stringify(data);
+        options.headers['Content-Type'] = 'application/json';
+    }
+
+    const token = Cookies.get('user');
+    if (token) {
+        options.headers['Authorization'] = `${token}`;
+    }
+
+    return options;
+};
+
+const request = async (method, url, data) => {
+    const response = await fetch(url, {
+        method,
+        ...options(data),
+    });
+
+    if (response.status === 204) {
+        return;
+    }
+    
+    let result;
+    if (response && typeof response == 'object') {
+        result = await response.json();
+    } else {
+        result = await response;
+    }
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result;
+};
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const patch = request.bind(null, 'PATCH');
+export const del = request.bind(null, 'DELETE');
