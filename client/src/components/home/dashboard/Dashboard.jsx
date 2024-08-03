fe5e54781f3ae51dff98271e55335e91448d3ea9
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 01:37:58 +0300

    Update Dashboard.jsx

--- client/src/components/home/dashboard/Dashboard.jsx
+++ client/src/components/home/dashboard/Dashboard.jsx
@@ -2,7 +2,8 @@ import MainTop from "./main-top/MainTop.jsx";
 import MainService from "./main-service/MainService.jsx";
 import MainPinned from "./main-pinned/MainPinned.jsx";
 import MainRated from "./main-rated/MainRated.jsx";
-
+import MainTheater from "./main-theater/MainTheater.jsx";
+import MainConcert from "./main-concert/MainConcert.jsx";
 
 
 export default function Dashboard() {
@@ -12,6 +13,8 @@ export default function Dashboard() {
             <MainService />
             <MainPinned />
             <MainRated />
+            <MainTheater />
+            <MainConcert />
 
         </>
     )
