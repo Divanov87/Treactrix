ab3ea8c41a921ab64789412df1f428addb36e62c
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 02:22:01 +0300

    Added Event Catalog page

---
+++ client/src/components/events/event-catalog/EventCatalog.jsx
@@ -0,0 +1,57 @@
+import EventCard from '../event-card/EventCard';
+import Loader from '../../loader/Loader';
+import { getAllEvents } from '../../../api/eventAPI';
+import { useEvents } from '../../../hooks/useEvents';
+import { usePagination } from '../../../hooks/usePagination';
+import { usePaginationQuery } from '../../../hooks/usePaginatonQuery';
+import Pagination from '../../../utils/Pagination';
+
+import styles from '../../home/search/Search.module.css';
+
+export default function EventCatalog() {
+  const [events, isLoading] = useEvents(getAllEvents);
+  const eventsPerPage = 8;
+
+  const initialPage = usePaginationQuery(1);
+  const {
+    currentPage,
+    totalPages,
+    currentEvents,
+    handlePageChange
+  } = usePagination(events, eventsPerPage, initialPage);
+
+  return (
+    <article>
+      <section className={styles['top-rated']}>
+        <div className={styles['container']}>
+          <p className={styles['section-subtitle']}>Latest</p>
+          <h2 className={`${styles['h2']} ${styles['section-title']}`}>
+            Theater & Music <strong>Performances</strong>
+          </h2>
+          {isLoading
+            ? <Loader />
+            : (
+              <>
+                <ul className={styles['movies-list']}>
+                  {currentEvents.length > 0 ? (
+                    currentEvents.map((event) => (
+                      <EventCard key={event._id} {...event} />
+                    ))
+                  ) : (
+                    <h2 className={`${styles['h2']} ${styles['section-title']}`}>
+                      No events found!
+                    </h2>
+                  )}
+                </ul>
+                <Pagination
+                  totalPages={totalPages}
+                  currentPage={currentPage}
+                  onPageChange={handlePageChange}
+                />
+              </>
+            )}
+        </div>
+      </section>
+    </article>
+  );
+}
