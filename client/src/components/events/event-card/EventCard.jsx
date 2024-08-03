e7e25314e57c9e9cb480f5cc07184540a6e9620d
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-03 22:47:40 +0300

    Added EventCard component

---
+++ client/src/components/events/event-card/EventCard.jsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+import styles from './EventCard.module.css';
+
+export default function EventCard({_id, imageUrl, name, location, date, duration, rating }) {
+  return (
+    <li>
+      <div className={styles['movie-card']}>
+        <Link to={`/events/${_id}/details`}>
+          <figure className={styles['card-banner']}>
+            <img src={imageUrl} alt={name} />
+          </figure>
+        </Link>
+        <div className={styles['title-wrapper']}>
+          <Link to={`/events/${_id}/details`}>
+            <h3 className={styles['card-title']}>{name.toUpperCase()}</h3>
+          </Link>
+          <div className={styles['location-badge']} style={{ textTransform: 'capitalize' }}>
+            <i className="bx bxs-map"></i>&nbsp;{location}
+          </div>
+        </div>
+        <div className={`${styles['badge']} ${styles['badge-outline']}`}>
+          <div className={styles['date-time']}>
+            <div>
+              <i className="bx bx-calendar"></i>
+              <time dateTime={date}>{new Date(date).toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' })}</time>
+            </div>
+            <div className={styles['duration']}>
+              <i className="bx bx-time"></i>
+              <time dateTime={duration}>{duration} min</time>
+            </div>
+            <div className={styles['rating']}>
+              <i className="bx bxs-star"></i>
+              <data>{rating}</data>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
