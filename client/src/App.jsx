71f9b96c7b5db2ccc5e671d8835db6c60e5f80cf
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 01:40:58 +0300

    Added experiment lazy loading for components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
-
+import { AuthProvider } from './context/AuthContext.jsx';
 
 
 
 import Dashboard from './components/home/dashboard/Dashboard.jsx';
 import ScrollToTop from './components/scroll-to-top/ScrolToTop';
 import Footer from "./components/footer/Footer.jsx";
-import { AuthProvider } from './context/AuthContext.jsx';
+
 import PWABadge from './PWABadge.jsx'
 
 
@@ -16,11 +17,10 @@ import PWABanner  from './components/pwa-banner/PWABanner.jsx';
 
 import Header from "./components/header/Header.jsx";
 
-import Register from './components/auth/register/Register';
-import Login from "./components/auth/login/Login.jsx";
+const Register = lazy(() => import('./components/auth/register/Register'));
+const Login = lazy(() => import('./components/auth/login/Login'));
 
 import NotFound from "./components/not-found/NotFound.jsx";
-
 
 export default function App() {
 
@@ -28,6 +28,7 @@ export default function App() {
     <>
         <AuthProvider>
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/auth/register" element={<Register />} />
@@ -35,6 +36,7 @@ export default function App() {
         <Route path="/404" element={<NotFound />} />
         <Route path='*' element={<Navigate to={'/404'} />} />
       </Routes>
+      </Suspense>
       <PWABanner/>
       <PWABadge />
       <ScrollToTop/>
