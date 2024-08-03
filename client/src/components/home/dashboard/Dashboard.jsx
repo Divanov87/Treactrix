6422c68040d2b7c58ebc1ef5c6fc49deb171d1ba
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 02:49:35 +0300

    Updated Dashboard with new sections

--- client/src/components/home/dashboard/Dashboard.jsx
+++ client/src/components/home/dashboard/Dashboard.jsx
@@ -2,8 +2,9 @@ import MainTop from "./main-top/MainTop.jsx";
 import MainService from "./main-service/MainService.jsx";
 import MainPinned from "./main-pinned/MainPinned.jsx";
 import MainRated from "./main-rated/MainRated.jsx";
-import MainTheater from "./main-theater/MainTheater.jsx";
 import MainConcert from "./main-concert/MainConcert.jsx";
+import MainTheater from "./main-theater/MainTheater.jsx";
+import EventItem from "../../event-list/event-item/EventItem.jsx";
 
 
 export default function Dashboard() {
@@ -11,11 +12,11 @@ export default function Dashboard() {
         <>
             <MainTop />
             <MainService />
-            <MainPinned />
-            <MainRated />
+            {/* <MainPinned /> */}
+            {/* <MainRated />
             <MainTheater />
-            <MainConcert />
-
+            <MainConcert /> */}
+            <EventItem />
         </>
     )
 }
