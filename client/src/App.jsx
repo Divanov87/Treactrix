262576f65a0bee7d400a798acb9b4582afaa3994
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 03:07:07 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ const Login = lazy(() => import('./components/auth/login/Login'));
 import NotFound from "./components/not-found/NotFound.jsx";
 import EventCatalog from './components/events/event-catalog/EventCatalog.jsx';
 import EventTheater from './components/events/event-theater/EventTheater.jsx';
-// import EventConcerts from './components/events/event-concerts/EventConcerts.jsx';
+import EventConcerts from './components/events/event-concerts/EventConcerts.jsx';
 
 export default function App() {
 
@@ -39,7 +39,7 @@ export default function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/events" element={<EventCatalog />} />
               <Route path="/events/theater" element={<EventTheater />} />
-              {/* <Route path="/events/concerts" element={<EventConcerts />} /> */}
+              <Route path="/events/concerts" element={<EventConcerts />} />
               <Route path="/auth/register" element={<Register />} />
               <Route path="/auth/login" element={<Login />} />
               <Route path="/404" element={<NotFound />} />
