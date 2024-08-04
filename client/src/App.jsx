e916940e63599ff9056646f07285fa3ff8e001a9
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-04 18:39:55 +0300

    Added lazy loading on appropriate routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,82 @@
 import { Suspense, lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
 import { AuthProvider } from './context/AuthContext.jsx';
 import { SearchProvider } from './context/SearchContext.jsx';
-
-import EventEdit from './components/events/event-edit/EventEdit.jsx';
-
 
 import AuthGuard from './guards/AuthGuard.jsx';
 import AuthUser from './guards/AuthUser.jsx';
 import AuthAdmin from './guards/AuthAdmin.jsx';
 
-
-
-import Search from './components/home/search/Search.jsx';
-import Dashboard from './components/home/dashboard/Dashboard.jsx';
-import ScrollToTop from './components/scroll-to-top/ScrolToTop';
-import Footer from "./components/footer/Footer.jsx";
-
-import PWABadge from './PWABadge.jsx'
-
-
-import PWABanner from './components/pwa-banner/PWABanner.jsx';
-
-
 import Header from "./components/header/Header.jsx";
+import Dashboard from './components/home/dashboard/Dashboard';
+
+const EventCatalog = lazy(() => import('./components/events/event-catalog/EventCatalog'));
+const EventTheater = lazy(() => import('./components/events/event-theater/EventTheater'));
+const EventConcerts = lazy(() => import('./components/events/event-concerts/EventConcerts'));
+const EventDetails = lazy(() => import('./components/events/event-details/EventDetails'));
+const EventAdd = lazy(() => import('./components/events/event-add/EventAdd'));
+const EventEdit = lazy(() => import('./components/events/event-edit/EventEdit'));
+
+const NotFound = lazy(() => import('./components/not-found/NotFound'));
 
 const Register = lazy(() => import('./components/auth/register/Register'));
 const Login = lazy(() => import('./components/auth/login/Login'));
 
-import NotFound from "./components/not-found/NotFound.jsx";
-import EventCatalog from './components/events/event-catalog/EventCatalog.jsx';
-import EventTheater from './components/events/event-theater/EventTheater.jsx';
-import EventConcerts from './components/events/event-concerts/EventConcerts.jsx';
-import EventAdd from './components/events/event-add/EventAdd.jsx';
-import EventDetails from './components/events/event-details/EventDetails.jsx';
-import Users from './components/home/users/Users.jsx';
-import Profile from './components/home/profile/Profile.jsx';
+const Search = lazy(() => import('./components/home/search/Search'));
+const Profile = lazy(() => import('./components/home/profile/Profile'));
+const Users = lazy(() => import('./components/home/users/Users'));
+// const Contacts = lazy(() => import('./components/home/contacts/Contacts')); //TODO
+// const About = lazy(() => import('./components/home/about/About')); //TODO
+
+import PWABanner  from './components/pwa-banner/PWABanner.jsx';
+import PWABadge from './PWABadge.jsx'
+import ScrollToTop from './components/scroll-to-top/ScrolToTop';
+import Footer from "./components/footer/Footer.jsx";
+
 
 export default function App() {
 
   return (
     <>
-      <AuthProvider>
-        <SearchProvider>
-          <Header />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/events" element={<EventCatalog />} />
-              <Route path="/events/theater" element={<EventTheater />} />
-              <Route path="/events/concerts" element={<EventConcerts />} />
-              <Route path="/events/:eventId/details" element={<EventDetails />} />
-              <Route path="/auth/register" element={<Register />} />
-              <Route path="/auth/login" element={<Login />} />
-              <Route path="/404" element={<NotFound />} />
-              <Route path='*' element={<Navigate to={'/404'} />} />
+    <AuthProvider>
+    <SearchProvider>
+      <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/events" element={<EventCatalog />} />
+          <Route path="/events/theater" element={<EventTheater />} />
+          <Route path="/events/concerts" element={<EventConcerts />} />
+          <Route path="/events/:eventId/details" element={<EventDetails />} />
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login />} />
+          {/* <Route path="/contacts" element={<Contacts />} />
+          <Route path="/about" element={<About />} /> */}
+          <Route path="/404" element={<NotFound />} />
+          <Route path='*' element={<Navigate to={'/404'} />} />
 
+          <Route element={<AuthGuard />}>
+          <Route path="/search" element={<Search />} />
+          </Route>
 
-              <Route element={<AuthGuard />}>
-                <Route path="/search" element={<Search />} />
-              </Route>
+          <Route element={<AuthUser />}>
+          <Route path="/profile" element={<Profile />} />
+          </Route>
 
+          <Route element={<AuthAdmin />}>
+          <Route path="/users" element={<Users />} />
+          <Route path="/events/add" element={<EventAdd />} />
+          <Route path="/events/:id/edit" element={<EventEdit />} />
+          </Route>
 
-              <Route element={<AuthUser />}>
-                <Route path="/profile" element={<Profile />} />
-              </Route>
-
-
-              <Route element={<AuthAdmin />}>
-                <Route path="/users" element={<Users />} />
-                <Route path="/events/add" element={<EventAdd />} />
-                <Route path="/events/:id/edit" element={<EventEdit />} />
-              </Route>
-
-
-            </Routes>
-          </Suspense>
-          <PWABanner />
-          <PWABadge />
-          <ScrollToTop />
-          <Footer />
-        </SearchProvider>
+          </Routes>
+        </Suspense>
+        <PWABanner/>
+      <PWABadge />
+      <ScrollToTop/>
+      <Footer />
+      </SearchProvider>
       </AuthProvider>
     </>
   )
