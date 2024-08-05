d72ccfa19a70d69d7e4803042a16acd8f2a96b82
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-05 23:36:41 +0300

    Preparing server for newest comment system

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
 const eventsController = require('./controllers/eventsController');
+const commentController = require('./controllers/commentController');
 
 router.use('/', homeController);
 router.use('/auth', authController);
 router.use('/events', eventsController);
+router.use('/comments', commentController);
 
 router.use('*', (req, res) => {
     res.status(404).json({ error: '404 Not Found' });
