54476979d1632d2ea667bd8bdcbeb71182fe09b1
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-06 03:15:14 +0300

    Separeted user comment system

--- client/src/components/events/event-details/EventDetails.jsx
+++ client/src/components/events/event-details/EventDetails.jsx
@@ -4,7 +4,7 @@ import { useParams, useLocation, useNavigate, Link } from 'react-router-dom';
 import QRCode from 'react-qr-code';
 import Swal from 'sweetalert2';
 
-import { formatDate, formatDateAdmin } from '../../../libs/dateFormatter';
+import { formatDate, formatDateAdmin } from '../../../libs/dateFormatter.js';
 
 import {
   getEvent,
@@ -15,14 +15,15 @@ import {
   pinEvent,
   unlikeEvent,
   unpinEvent,
-} from '../../../api/eventAPI';
+} from '../../../api/eventAPI.js';
 
 import { getComment, addComment, deleteComment, updateComment } from '../../../api/commentAPI.js';
 
-import { useAuth } from '../../../context/AuthContext';
+import { useAuth } from '../../../context/AuthContext.jsx';
 
-import Loader from '../../loader/Loader';
-import EventMeta from '../event-details/event-meta/EventMeta'
+import Loader from '../../loader/Loader.jsx';
+import EventMeta from './event-meta/EventMeta.jsx'
+import EventComments from './event-comments/EventComments.jsx';
 
 import styles from './EventDetails.module.css';
 
@@ -57,20 +58,9 @@ export default function EventDetail() {
       }
     };
 
-
-    // const fetchComments = async () => {
-    //   try {
-    //     const response = await fetch(`http://192.168.56.1:3000/comments/${eventId}`);
-    //     const data = await response.json();
-    //     setComments(data);
-    //   } catch (error) {
-    //     console.error('Error fetching comments:', error);
-    //   }
-    // };
-
     const getComments = async () => {
       try {
-          const data = await getComment(eventId); // Use the getComment function from your api
+          const data = await getComment(eventId);
           setComments(data);
       } catch (error) {
           console.error('Error fetching comments:', error);
@@ -79,41 +69,9 @@ export default function EventDetail() {
 
 
     fetchEvent();
-    getComments();
-  
+
   }, [eventId]);
 
-  // const handleCommentSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (!newComment.trim()) return;
-
-  //   try {
-  //     const response = await fetch(`http://192.168.56.1:3000/comments/${eventId}`, {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ userId, text: newComment }),
-  //     });
-
-  //     const newCommentData = await response.json();
-  //     setComments(prevComments => [...prevComments, newCommentData]);
-  //     setNewComment('');
-  //   } catch (error) {
-  //     console.error('Error adding comment:', error);
-  //   }
-  // };
-
-    // const handleCommentDelete = async (commentId) => {
-  //   try {
-  //     await fetch(`http://192.168.56.1:3000/comments/${commentId}`, {
-  //       method: 'DELETE',
-  //     });
-  //     setComments(comments.filter(comment => comment._id !== commentId));
-  //   } catch (error) {
-  //     console.error('Error deleting comment:', error);
-  //   }
-  // };
 
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
@@ -129,7 +87,6 @@ export default function EventDetail() {
         console.error('Error adding comment:', error);
     }
 };
-
 
 
   const handleCommentDelete = async (commentId) => {
@@ -167,15 +124,6 @@ const handleEditSubmit = async (e) => {
     const maskedName = name.slice(0, 2) + '***' + name.slice(-1);
     return `${maskedName}@${domain}`;
   };
-
-  // const maskEmail = (email) => {
-  //   if (!email) return 'email not available';
-  //   const [name, domain] = email.split('@');
-  //   const maskedName = name.slice(0, 2) + '***' + name.slice(-1);
-  //   return `${maskedName}@${domain}`;
-  // };
-
-
 
   const CloneEvent = async () => {
     if (user?.role === 'admin') {
@@ -571,352 +519,11 @@ const handleEditSubmit = async (e) => {
                 </span>
               </div>
             </div>
-
-
-
-
-
-
-
-
-
-            <section className='container'>
-            <article className={styles['event-detail']}>
-              <p className={styles['section-subtitle']}>Comments</p>
-              <h2 className={`${styles['h2']} ${styles['section-title']}`}>
-                WHAT PEOPLE ARE <strong>SAYING?</strong>
-              </h2>
-      
-              {isLogged ? (
-                <form onSubmit={handleCommentSubmit} className={styles.commentForm}>
-                  <textarea
-                    value={newComment}
-                    onChange={(e) => setNewComment(e.target.value)}
-                    placeholder="Add a comment..."
-                    required
-                  />
-                  <button type="submit" className={styles['comment-button']}>Send</button>
-                </form>
-              ) : (
-                <p>Please <Link to="/auth/login">login</Link> to add a comment.</p>
-              )}
-      
-              <ul className={styles['commentList']}>
-                {comments.map((comment) => (
-                  <li key={comment._id} className={styles['comment']}>
-                    <div>
-                      <button className={styles['comment-delete']}>
-                        {`${comment.author.username} (${maskEmail(comment.author.email)})`}
-                      </button>
-                      <p>{comment.text}</p>
-                      <i>{formatDateAdmin(comment.createdAt)}</i>
-                      {(userId === comment.author._id || user?.role === 'admin') && (
-                        <>
-                          {editingComment && editingComment._id === comment._id ? (
-                            <form onSubmit={handleEditSubmit}>
-                              <textarea
-                                value={editedText}
-                                onChange={(e) => setEditedText(e.target.value)}
-                                required
-                              />
-                              <button type="submit" className={styles['comment-delete']} >Update</button>&nbsp;
-                              <button type="button" onClick={() => setEditingComment(null)} className={styles['comment-delete']} >Cancel</button>
-                            </form>
-                          ) : (
-                            <>
-                              <button className={styles['comment-delete']} onClick={() => startEditing(comment)}>Edit</button>
-                              &nbsp;
-                              <button className={styles['comment-delete']} onClick={() => handleCommentDelete(comment._id)}>Delete</button>
-                            </>
-                          )}
-                        </>
-                      )}
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </article>
-          </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          </>
-        }
-      </section>
-      {!isLoading && <EventMeta />}
-    </article>
-  );
-}
-
-
-
-// import { useState, useEffect } from 'react';
-// import { useParams, useNavigate, Link } from 'react-router-dom';
-// import QRCode from 'react-qr-code';
-// import Swal from 'sweetalert2';
-// import {
-//   getEvent,
-//   cloneEvent,
-//   deleteEvent,
-//   buyTickets,
-//   likeEvent,
-//   pinEvent,
-//   unlikeEvent,
-//   unpinEvent,
-// } from '../../../api/eventAPI';
-// import { useAuth } from '../../../context/AuthContext';
-// import Loader from '../../loader/Loader';
-// import EventMeta from '../event-details/event-meta/EventMeta';
-// import styles from './EventDetails.module.css';
-
-// export default function EventDetail() {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [event, setEvent] = useState(null);
-//   const [showAll, setShowAll] = useState(false);
-//   const navigate = useNavigate();
-//   const { user, isLogged } = useAuth();
-//   const { eventId } = useParams();
-//   const userId = user?._id;
-//   const eventData = { eventId, userId };
-
-//   useEffect(() => {
-//     const fetchEvent = async () => {
-//       try {
-//         const fetchedEvent = await getEvent(eventId);
-//         setEvent(fetchedEvent);
-//       } catch (error) {
-//         console.error('Error fetching event:', error);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchEvent();
-//   }, [eventId]);
-
-//   const Action = async (action, successMessage) => {
-//     try {
-//       await action(eventData);
-//       const updatedEvent = await getEvent(eventId);
-//       setEvent(updatedEvent);
-//       Swal.fire(successMessage, '', 'success');
-//     } catch (error) {
-//       console.error(`Error performing action: ${error}`);
-//       Swal.fire(`Error performing action: ${error}`, '', 'error');
-//     }
-//   };
-
-//   const Dialog = async (action, title, successMessage) => {
-//     const result = await Swal.fire({
-//       title,
-//       icon: 'question',
-//       showCancelButton: true,
-//       confirmButtonText: 'Yes, proceed',
-//     });
-
-//     if (result.isConfirmed) {
-//       Action(action, successMessage);
-//     }
-//   };
-
-//   const CloneEvent = async () => {
-//     if (user?.role === 'admin') {
-//       const result = await Swal.fire({
-//         title: 'Confirm cloning event?',
-//         icon: 'question',
-//         showCancelButton: true,
-//         confirmButtonText: 'Yes, clone event',
-//       });
-
-//       if (result.isConfirmed) {
-//         try {
-//           const response = await cloneEvent(eventId);
-//           Swal.fire('Cloned successfully!', response, 'success');
-//           navigate('/events');
-//         } catch (error) {
-//           console.error('Error cloning event:', error);
-//           Swal.fire('Error cloning event', error.message, 'error');
-//         }
-//       }
-//     } else {
-//       Swal.fire('No rights to clone events!', '', 'error');
-//     }
-//   };
-
-//   const Share = async () => {
-//     const shareData = {
-//       title: 'Theatrix',
-//       url: window.location.href,
-//     };
-
-//     try {
-//       if (navigator.share) {
-//         await navigator.share(shareData);
-//         console.log('Content shared successfully');
-//       } else {
-//         const shareMessage = `Share this content: ${shareData.url}`;
-//         if (window.confirm(shareMessage)) {
-//           alert('To share this content, copy the URL and share it manually.');
-//         }
-//       }
-//     } catch (error) {
-//       console.error('Error sharing content:', error);
-//     }
-//   };
-
-//   const ReadMore = () => setShowAll(!showAll);
-
-//   return (
-//     <article className={styles['event-detail']}>
-//       <section className={styles['movie-detail']}>
-//         <p className={styles['section-subtitle']}>Events</p>
-//         <h2 className={`${styles['h2']} ${styles['section-title']}`}>
-//           {event?.category?.toUpperCase()} <strong>PERFORMANCE</strong>
-//         </h2>
-//         {isLoading ? (
-//           <Loader />
-//         ) : (
-//           <div className={styles['container']}>
-//             <figure className={styles['movie-detail-banner']}>
-//               <img src={event?.imageUrl} alt={event?.name} />
-//             </figure>
-//             <div className={styles['movie-detail-content']}>
-//               <p className={styles['detail-subtitle']}>
-//                 <b className="badge badge-fill">{event?.genre}</b> <b className="badge badge-outline">{event?.restriction}+</b>
-//               </p>
-//               <h1 className={`${styles['h1']} ${styles['detail-title']}`} style={{ textTransform: 'uppercase' }}>
-//                 {event?.name}
-//               </h1>
-//               <div className={styles['meta-wrapper']}>
-//                 <div className={styles['date-time']}>
-//                   <div className={styles['location-badge']} style={{ textTransform: 'capitalize' }}>
-//                     <i className="bx bxs-map"></i>
-//                     <time>{event?.location}</time>
-//                   </div>
-//                   |
-//                   <div>
-//                     <i className="bx bx-calendar"></i>
-//                     <time dateTime={event?.date}>
-//                       {new Date(event?.date).toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' })}
-//                     </time>
-//                   </div>
-//                   |
-//                   <div>
-//                     <i className="bx bx-time"></i>
-//                     <time>{event?.time}</time>
-//                   </div>
-//                   |
-//                   <div>
-//                     <i className="bx bx-timer"></i>
-//                     <time>{event?.duration} min</time>
-//                   </div>
-//                 </div>
-//               </div>
-//               <p className={styles['storyline']}>
-//                 {showAll ? event?.description : `${event?.description?.slice(0, 250)}...`}
-//                 {!showAll && <button onClick={ReadMore}>Read More</button>}
-//               </p>
-//               {isLogged && (
-//                 <div className={styles['details-actions']}>
-//                   {user?.role === 'admin' && (
-//                     <>
-//                       <Link to={`/events/${event?._id}/edit`}>
-//                         <button className="share">
-//                           <i className={`bx bx-edit-alt bx-tada-hover admin ${styles['box-details']}`}></i>
-//                           <span>Edit</span>
-//                         </button>
-//                       </Link>
-//                       {event?.pinsList?.length > 0 ? (
-//                         <button className="share" onClick={() => Dialog(unpinEvent, 'Confirm removing event from home page?', 'Event unpinned successfully!')}>
-//                           <i className={`bx bxs-star bx-spin admin ${styles['box-details']}`}></i>
-//                           <span>Pinned!</span>
-//                         </button>
-//                       ) : (
-//                         <button className="share" onClick={() => Dialog(pinEvent, 'Confirm adding this event to home page?', 'Event pinned successfully!')}>
-//                           <i className={`bx bx-star bx-spin-hover admin ${styles['box-details']}`}></i>
-//                           <span>Pin</span>
-//                         </button>
-//                       )}
-//                       {!event?.name?.includes('_') ? (
-//                         <button className="share" onClick={CloneEvent}>
-//                           <i className={`bx bx-copy-alt admin ${styles['box-details']}`}></i>
-//                           <span>Clone</span>
-//                         </button>
-//                       ) : (
-//                         <button className="share">
-//                           <i className={`bx bxs-error bx-flashing admin ${styles['box-details']}`}></i>
-//                           <span>Cloned!</span>
-//                         </button>
-//                       )}
-//                       <button className="share">
-//                         <i className={`bx bxs-coupon admin admin-coupon ${styles['box-details']}`}></i>
-//                         <span>{event?.ticketsLeft}</span>
-//                       </button>
-//                       <button className="share" onClick={() => Dialog(deleteEvent, `DELETE "${event?.name}"?`, 'Event deleted successfully')}>
-//                         <i className={`bx bx-trash admin ${styles['box-details']}`}></i>
-//                         <span>Delete</span>
-//                       </button>
-//                     </>
-//                   )}
-//                   {user?.role === 'user' && (
-//                     <>
-//                       <button className="share" onClick={Share}>
-//                         <i className={`bx bx-share-alt bx-tada-hover ${styles['box-details']}`}></i>
-//                         <span>Share</span>
-//                       </button>
-//                       {!event?.isLiked ? (
-//                         <button className="share" onClick={() => Action(likeEvent, 'Event liked!')}>
-//                           <i className={`bx bx-heart bx-burst-hover ${styles['box-details']}`}></i>
-//                           <span>Like</span>
-//                         </button>
-//                       ) : (
-//                         <button className="share" onClick={() => Action(unlikeEvent, 'Event unliked!')}>
-//                           <i className={`bx bxs-heart user-heart ${styles['box-details']}`}></i>
-//                           <span>{event?.likesList?.length}</span>
-//                         </button>
-//                       )}
-//                       {event?.ticketsLeft > 0 ? (
-//                         <button className={`${styles['btn']} ${styles['btn-primary']}`} onClick={() => Dialog(buyTickets, 'Confirm buying tickets', 'Tickets bought successfully!')}>
-//                           Buy Tickets
-//                         </button>
-//                       ) : (
-//                         <button className={styles['sold-out']}>
-//                           Sold out
-//                         </button>
-//                       )}
-//                     </>
-//                   )}
-//                 </div>
-//               )}
-//             </div>
-//             <div className={styles['download-btn']}>
-//               <QRCode title="Send your favourite event quickly to your mobile!" value={window.location.href} bgColor="transparent" fgColor="#e60a15" size={125} />
-//               <span className={styles['qr']}>
-//                 <span>Scan me!</span>
-//               </span>
-//             </div>
-//           </div>
-//         )}
-//       </section>
-//       {!isLoading && <EventMeta />}
-//     </article>
-//   );
-// }
-
+            </>
+          }
+        </section>
+        {!isLoading && <EventComments />}
+        {!isLoading && <EventMeta />}
+      </article>
+    );
+  };
---
+++ client/src/components/events/event-details/event-comments/EventComments.jsx
@@ -0,0 +1,161 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+
+import { formatDateAdmin } from '../../../../libs/dateFormatter.js';
+import { getComment, addComment, deleteComment, updateComment } from '../../../../api/commentAPI.js';
+import { useAuth } from '../../../../context/AuthContext.jsx';
+import Loader from '../../../loader/Loader.jsx';
+
+import styles from './EventComments.module.css';
+
+
+export default function EventComments() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [showAll, setShowAll] = useState(false);
+
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const [editingComment, setEditingComment] = useState(null);
+  const [editedText, setEditedText] = useState('');
+
+  const navigate = useNavigate();
+  const { user, isLogged } = useAuth();
+  const userId = user?._id;
+  const { eventId } = useParams();
+
+  const eventData = { eventId, userId };
+
+  useEffect(() => {
+    
+    const getComments = async () => {
+      try {
+          const data = await getComment(eventId);
+          setComments(data);
+      } catch (error) {
+          console.error('Error fetching comments:', error);
+      }
+  };
+
+    getComments();
+  
+  }, [eventId]);
+
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+    if (!newComment.trim()) return;
+
+    try {
+        const commentData = { userId, text: newComment };
+        const newCommentData = await addComment(eventId, commentData); 
+
+        setComments(prevComments => [...prevComments, newCommentData]);
+        setNewComment('');
+    } catch (error) {
+        console.error('Error adding comment:', error);
+    }
+};
+
+
+
+  const handleCommentDelete = async (commentId) => {
+    try {
+        await deleteComment(eventId, commentId);
+        setComments(prevComments => prevComments.filter(comment => comment._id !== commentId));
+    } catch (error) {
+        console.error('Error deleting comment:', error);
+    }
+};
+
+const startEditing = (comment) => {
+  setEditingComment(comment);
+  setEditedText(comment.text);
+};
+
+const handleEditSubmit = async (e) => {
+  e.preventDefault();
+  if (!editedText.trim()) return;
+
+  try {
+      await updateComment(editingComment._id, { text: editedText });
+      const updatedComments = await getComment(eventId);
+      setComments(updatedComments);
+      setEditingComment(null);
+      setEditedText('');
+  } catch (error) {
+      console.error('Error updating comment:', error);
+  }
+};
+
+
+  const maskEmail = (email) => {
+    const [name, domain] = email.split('@');
+    const maskedName = name.slice(0, 2) + '***' + name.slice(-1);
+    return `${maskedName}@${domain}`;
+  };
+
+  return (
+    <section className='container'>
+    <article className={styles['event-detail']}>
+      <p className={styles['section-subtitle']}>Comments</p>
+      <h2 className={`${styles['h2']} ${styles['section-title']}`}>
+        WHAT PEOPLE ARE <strong>SAYING?</strong>
+      </h2>
+
+      {isLogged ? (
+        <form onSubmit={handleCommentSubmit} className={styles['comment-form']}>
+          <textarea
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Add a comment..."
+            required
+          />
+          <button type="submit" className={styles['comment-button']}>Send</button>
+        </form>
+      ) : (
+        <p>Please <Link to="/auth/login">login</Link> to add a comment.</p>
+      )}
+
+{comments.length === 0 ? (
+
+        <h2 className={`${styles['h2']} ${styles['section-title']}`}>No comments <strong>yet..</strong> <br/><strong><strike>Love it?</strike></strong> Hate it? Be the first to <strong>share your thoughts!</strong></h2>
+      ) : (
+      <ul className={styles['commentList']}>
+        {comments.map((comment) => (
+          <li key={comment._id} className={styles['comment']}>
+            <div>
+              <button className={styles['comment-delete']}>
+                {`${comment.author.username} (${maskEmail(comment.author.email)})`}
+              </button>
+              <p>{comment.text}</p>
+              <i>{formatDateAdmin(comment.createdAt)}</i>
+              {(userId === comment.author._id || user?.role === 'admin') && (
+                <>
+                  {editingComment && editingComment._id === comment._id ? (
+                    <form onSubmit={handleEditSubmit}>
+                      <textarea
+                        value={editedText}
+                        onChange={(e) => setEditedText(e.target.value)}
+                        required
+                      />
+                      <button type="submit" className={styles['comment-delete']} >Update</button>&nbsp;
+                      <button type="button" onClick={() => setEditingComment(null)} className={styles['comment-delete']} >Cancel</button>
+                    </form>
+                  ) : (
+                    <>
+                      <button className={styles['comment-delete']} onClick={() => startEditing(comment)}>Edit</button>
+                      &nbsp;
+                      <button className={styles['comment-delete']} onClick={() => handleCommentDelete(comment._id)}>Delete</button>
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>)}
+    </article>
+  </section>
+  );
+}
