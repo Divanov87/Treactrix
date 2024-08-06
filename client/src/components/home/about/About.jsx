2b1c26a737bd91bb45409d1ac5a20959d181340d
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-08-07 02:22:07 +0300

    Added about static view

---
+++ client/src/components/home/about/About.jsx
@@ -0,0 +1,89 @@
+import './About.css';
+
+export default function About() {
+  return (
+    <article>
+      <section className="movie-detail">
+        <p className="section-subtitle">about</p>
+        <h2 className="h2 section-title">About <strong>Us</strong></h2>
+        <div className="container">
+
+          <div className="wrapper about-us">
+            <div className="container container-reg-log-edit">
+              <div className="content">
+                <h1>Welcome to <strong>Theatrix</strong>,</h1>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <div className="big-word">we have</div>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>diverse</h2> array of hundreds
+                  <h2 style={{ color: "white" }}>events</h2>
+                </div>
+                <div className="big-word">ranging</div>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>from <strong>Broadway shows</strong></h2>
+                </div>
+                <div className="big-word"> to intimate concerts</div>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>we're here to</h2>
+                </div>
+                <div className="big-word">ignite your passion
+                  <h2 style={{ color: "white" }}>for the</h2>
+                  <strong>performing arts,</strong>
+                </div>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>and create</h2>
+                </div>
+                <div className="big-word">
+                  <strong>unforgettable
+                    <h2 style={{ color: "white" }}>memories!</h2>
+                  </strong>
+                </div>
+                <p>&nbsp;</p>
+                <h2>Discovering Events Made Effortless</h2>
+                <p>Gone are the days of <strong>endless searching</strong> and <strong>countless tabs</strong> open on your browser. With <strong>Theatrix</strong>, finding the <strong>perfect event</strong> is a <strong>breeze</strong>. Our <strong>intuitive interface</strong> allows you to <strong>browse</strong> through a <strong>vast selection</strong> of <strong>performances</strong>, <strong>conveniently sorted</strong> by genre, date, location, and more. Whether you're in the mood for a</p>
+                <div className="big-word">classical
+                  <h2 style={{ color: "white" }}>ballet,</h2>
+                </div>
+                <p>a</p>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>dripping</h2>drama,
+                </div>
+                <p>or a night of</p>
+                <div className="big-word">
+                  <h2 style={{ color: "white" }}>stand-up</h2> comedy!
+                </div>
+                <p>our platform puts the <strong>world of entertainment</strong></p>
+                <div className="big-word" style={{ color: "white" }}>
+                  <h2>at your <strong>fingertips!</strong></h2>
+                </div>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <h2>Securing Your Tickets Hassle-Free</h2>
+                <p>&nbsp;</p>
+                <p>Once you've found the event that <strong>piques your interest</strong>, securing your tickets is <strong>quick and easy</strong> with <strong>Theatrix</strong>. Say goodbye to <strong>long queues</strong> and <strong>last-minute rushes</strong> – our <strong>seamless booking system</strong> ensures that you can <strong>reserve your seats</strong> with just a <strong>few clicks</strong>. Whether you prefer the <strong>front row</strong> or the <strong>balcony</strong>, we offer a range of <strong>seating options</strong> to suit every <strong>preference</strong> and <strong>budget</strong>. Plus, with our <strong>secure payment gateway</strong>, you can rest assured that your <strong>transactions</strong> are <strong>safe and protected</strong>.</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <h2>Immersing Yourself in the Experience</h2>
+                <p>&nbsp;</p>
+                <p>But <strong>Theatrix</strong> is more than just a <strong>ticketing platform</strong> – it's a <strong>gateway to unforgettable experiences</strong>. From the moment you <strong>purchase your tickets</strong> to the <strong>final curtain call</strong>, we're dedicated to ensuring that your <strong>journey</strong> is nothing short of <strong>magical</strong>. Explore our <strong>interactive event guides</strong>, <strong>insider tips</strong>, and <strong>behind-the-scenes content</strong> to <strong>enhance</strong> your <strong>understanding</strong> and <strong>appreciation</strong> of the performances you love. Connect with <strong>fellow enthusiasts</strong>, share your <strong>thoughts</strong> and <strong>experiences</strong>, and become part of a <strong>vibrant community</strong> united by a <strong>passion for the arts</strong>.</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <h2>Supporting Artists and Creators</h2>
+                <p>&nbsp;</p>
+                <p>At <strong>Theatrix</strong>, we believe in the <strong>power of creativity</strong> to <strong>inspire</strong>, <strong>uplift</strong>, and <strong>transform lives</strong>. That's why we're committed to supporting artists and creators from around the world, providing them with a <strong>platform</strong> to <strong>showcase their talents</strong> and reach new audiences. Every ticket purchased through our platform directly contributes to the <strong>livelihood</strong> of performers, production crews, and cultural institutions, ensuring that the <strong>magic of live entertainment</strong> continues to <strong>thrive for generations to come</strong>.</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <h2>Join the Theatrix Community Today</h2>
+                <p>&nbsp;</p>
+                <p>Whether you're a <strong>seasoned theatergoer</strong> or a newcomer to the world of <strong>live entertainment</strong>, there's something for everyone at <strong>Theatrix</strong>. Join us on a journey of <strong>discovery</strong>, <strong>exploration</strong>, and <strong>celebration</strong> as we embark on an exhilarating adventure through the world of <strong>performing arts</strong>. <strong>Sign up for free</strong> today and unlock access to <strong>exclusive offers</strong>, <strong>insider insights</strong>, and a world of <strong>unforgettable experiences</strong>. With <strong>Theatrix</strong>, the <strong>show never stops</strong> – so what are you waiting for? <strong>Start your adventure today!</strong></p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+};
